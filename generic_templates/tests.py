7bd1e69671954976a0cb02e8c57b1b5579efb8c3
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2011-06-10 06:45:06 -0700

    Reformat some strings so that lines are under 80 characters.

--- generic_templates/tests.py
+++ generic_templates/tests.py
@@ -37,13 +37,16 @@ class DetailHtmlFilterTest (TestCase):
         
         # Mock Django's get_template so that it doesn't load a real file;
         # instead just return a template that allows us to verify the context
-        gf.get_template = Mock(return_value=Template('{{ instance|safe }}:{{ fields|safe }}'))
+        gf.get_template = Mock(
+            return_value=Template('{{ instance|safe }}:{{ fields|safe }}'))
     
     
     def test_model_format(self):
         """Tests that a given model is formatted as expected."""
         
-        expected_detail = u"Pepulator #123456:[('serial number', 123456), ('height', 25), ('width', 16), ('manufacture date', %r), ('color', 'chartreuse')]" % self.m.manufacture_date
+        expected_detail = (u"Pepulator #123456:[('serial number', 123456),"
+              " ('height', 25), ('width', 16), ('manufacture date', %r),"
+              " ('color', 'chartreuse')]") % self.m.manufacture_date
         detail = gf.as_detail_html(self.m)
         
         gf.get_template.assert_called_with('object_detail.html')
@@ -53,10 +56,13 @@ class DetailHtmlFilterTest (TestCase):
     def test_filter_is_registered(self):
         """Test that the filter can be used from within a template"""
         
-        template = Template('{% load generic_filters %} {{ pepulator|as_detail_html }}')
+        template = Template(('{% load generic_filters %}'
+                             '{{ pepulator|as_detail_html }}'))
         context = Context({'pepulator':self.m})
         
-        expected_detail = u" Pepulator #123456:[('serial number', 123456), ('height', 25), ('width', 16), ('manufacture date', %r), ('color', 'chartreuse')]" % self.m.manufacture_date
+        expected_detail = (u"Pepulator #123456:[('serial number', 123456),"
+            " ('height', 25), ('width', 16), ('manufacture date', %r),"
+            " ('color', 'chartreuse')]") % self.m.manufacture_date
         detail = template.render(context)
         
         gf.get_template.assert_called_with('object_detail.html')

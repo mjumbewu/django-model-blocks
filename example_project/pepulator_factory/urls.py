75b518050c79fb6dbe2fba39e6661a4831068cd6
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2011-06-11 14:12:21 -0700

    Updated the urls and database to reflect recent model changes

--- example_project/pepulator_factory/urls.py
+++ example_project/pepulator_factory/urls.py
@@ -5,22 +5,22 @@ import pepulator_factory.models
 
 urlpatterns = patterns('pepulator_factory',
     url(r'^pepulators/$', ListView.as_view(
-        model=pepulator_factory.models.PepulatorModel,
+        model=pepulator_factory.models.Pepulator,
         template_name='pepulator_list.html',
         context_object_name='pepulators'),
         name='pepulator_list_view'),
     url(r'^pepulators/(?P<pk>\d+)/$', DetailView.as_view(
-        model=pepulator_factory.models.PepulatorModel,
+        model=pepulator_factory.models.Pepulator,
         template_name='pepulator_detail.html',
         context_object_name='pepulator'),
         name='pepulator_detail_view'),
     url(r'^distributors/$', ListView.as_view(
-        model=pepulator_factory.models.DistributorModel,
+        model=pepulator_factory.models.Distributor,
         template_name='distributor_list.html',
         context_object_name='distributors'),
         name='distributor_list_view'),
     url(r'^distributors/(?P<pk>.+)/$', DetailView.as_view(
-        model=pepulator_factory.models.DistributorModel,
+        model=pepulator_factory.models.Distributor,
         template_name='distributor_detail.html',
         context_object_name='distributor'),
         name='distributor_detail_view'),
